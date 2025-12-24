9d28a115db0640dbf0aab52dbc402dcca01891d7
Alex Mai <alexandertmai@gmail.com>
2025-12-24 12:14:10 -0800

    Fix some bugs

--- models/frozen.py
+++ models/frozen.py
@@ -185,7 +185,7 @@ class FrozenTetModel(BaseModel):
             sh       = self.sh
 
         sh_dim = (self.max_sh_deg+1)**2 - 1
-        attr = torch.empty((sh.shape[0], 0))
+        attr = torch.empty((sh.shape[0], 0), device=grd.device)
         return circumcenter, density, rgb, grd, sh.reshape(-1, sh_dim, 3), attr
 
     def compute_features(self, offset=False):
